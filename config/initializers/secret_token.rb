e9d9b2c70acb9e9020b0ea198ee0e30493ad0ed0
whitperson <whitperson@gmail.com>
2012-12-05 21:36:02 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jamlister::Application.config.secret_token = '4ab51f756fb9ac0529e12d2111acbc187d09f5b1b4310f017912fb762baae824889d1d4cee97c5a6b94e639b169e6bfc5b9c367d899d0755b8272f75881c6122'
