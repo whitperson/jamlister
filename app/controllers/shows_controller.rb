59655fc9b3d1a38531a702df6b67c35161ef7b55
whitperson <whitperson@gmail.com>
2012-12-18 17:05:54 -0500

    updated deleted files

--- app/controllers/shows_controller.rb
+++
@@ -1,4 +0,0 @@
-class ShowsController < ApplicationController
-  def index
-  end
-end
--- db/migrate/20121206040901_add_songs_artists_join_table.rb
+++
@@ -1,8 +0,0 @@
-class AddSongsArtistsJoinTable < ActiveRecord::Migration
-  def change
-       create_table :songs_artists, :id => false do |t|
-       t.integer :artist_id
-       t.integer :song_id
-    end
-  end
-end
